37772327aface5ca98de1ae6d17abb31d10724d4
gulabsingh98 <gulabsingh989888@gmail.com>
2024-08-25 12:41:57 +0530

    Create cfg.py

---
+++ cfg.py
@@ -0,0 +1,11 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7528157923:AAGRYRPKxf_geD_a6JRwM8-qZVSlCJ9v3DU")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("beatbeast007")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1340435264")
